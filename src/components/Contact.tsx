1411edd2cb814bac9c72ca692b814541d704493b
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-07-15 00:04:45 +0530

    multiple updates

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Contact:React.FC = () => {
   return (
-    <div className = "flex flex-col justify-center items-center gap-[90px] p-[150px] px-[40px] sm:px-[120px]">
-        <h1 className='text-[36px] font-bold'>Mail me on:</h1>
-        <p className='text-[30px]'>rkrohitkumar893@gmail.com</p>
+    <div className = "flex flex-col justify-center items-center gap-[20px] p-[150px] px-[40px] sm:px-[120px]">
+        <h1 className='text-[36px] font-bold'>CONTACT:</h1>
+        <p className='text-[22px] sm:text-[30px] flex gap-[8px]'><img src="mail_icon.png" className='h-[50px] mt-[-6px] sm:mt-0' />rkrohitkumar893@gmail.com</p>
     </div>
   )
 }
