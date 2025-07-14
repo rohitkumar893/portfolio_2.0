93cde98fa59c79aa26ecc03f1b31be1b2e7508d0
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-07-14 11:17:06 +0530

    add: logos

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,28 +1,64 @@
-import React from 'react'
+import React from 'react';
 
-const Skills:React.FC = () => {
-  return (
-    <>
-        <div className='h-full w-full bg-gray-900 flex justify-center items-center flex-col'>
-            <b className='text-4xl'>MY TECH STACK</b>
-            <br /><br />
-            <b className='text-3xl'>LANGUAGES</b>
-            <br />
-            <p className='text-3xl'>TYPESCRIPT</p>
-            <p className='text-3xl'>JAVASCRIPT</p>
-            <p className='text-3xl'>HTML</p>
-            <p className='text-3xl'>CSS</p>
-            <br /><br />
-            <b className='text-3xl'>TECHNOLOGIES</b>
-            <br />
-            <p className='text-3xl'>REACT</p>
-            <p className='text-3xl'>NODE JS</p>
-            <p className='text-3xl'>TAILWIND CSS</p>
-            <p className='text-3xl'>BUN</p>
-            <p className='text-3xl'>GIT</p>
+  const Skills: React.FC = () => {
+    return (
+      <div
+        className="skills text-[40px] w-full bg-gray-900 p-[110px] px-[40px] sm:px-[120px] flex flex-col gap-[40px]"
+        id="skills"
+      >
+        <h1 className="text-center text-[36px] font-bold">MY TECH STACK :</h1>
+
+        {/* LANGUAGES */}
+        <div className="languages">
+          <h3 className="text-center text-[32px] mt-[20px]">LANGUAGES</h3>
+
+          <div className="flex justify-center items-center gap-4 mt-[20px]">
+            <img src="typescript.png" className="h-[40px]" alt="TypeScript Logo" />
+            <h3 className="text-[30px]">TYPESCRIPT</h3>
+          </div>
+
+          <div className="flex justify-center items-center gap-4 mt-[20px]">
+            <img src="javascript.png" className="h-[40px]" alt="JavaScript Logo" />
+            <h3 className="text-[30px]">JAVASCRIPT</h3>
+          </div>
+
+          <div className="flex justify-center items-center gap-4 mt-[20px]">
+            <img src="html.png" className="h-[40px]" alt="HTML Logo" />
+            <h3 className="text-[30px]">HTML</h3>
+          </div>
+
+          <div className="flex justify-center items-center gap-4 mt-[20px]">
+            <img src="css.png" className="h-[40px]" alt="CSS Logo" />
+            <h3 className="text-[30px]">CSS</h3>
+          </div>
         </div>
-    </>
-  )
-}
 
-export default Skills
+        {/* TECHNOLOGIES */}
+        <div className="technologies">
+          <h3 className="text-center text-[32px] mt-[20px]">TECHNOLOGIES</h3>
+
+          <div className="flex justify-center items-center gap-4 mt-[20px]">
+            <img src="react.png" className="h-[40px]" alt="React Logo" />
+            <h3 className="text-[30px]">REACT</h3>
+          </div>
+
+          <div className="flex justify-center items-center gap-4 mt-[20px]">
+            <img src="Tailwind Logo.png" className="h-[28px]" alt="Tailwind Logo" />
+            <h3 className="text-[30px]">TAILWIND CSS</h3>
+          </div>
+
+          <div className="flex justify-center items-center gap-4 mt-[20px]">
+            <img src="nod.png" className="h-[40px] mt-1" alt="Node.js Logo" />
+            <h3 className="text-[30px]">NODE JS</h3>
+          </div>
+
+          <div className="flex justify-center items-center gap-4 mt-[20px]">
+            <img src="exp.png" className="h-[40px]" alt="Express Logo" />
+            <h3 className="text-[30px]">EXPRESS JS</h3>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+export default Skills;
