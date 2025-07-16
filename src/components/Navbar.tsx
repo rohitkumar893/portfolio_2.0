0354080a7acebca7eac9b1370bc16c7187111466
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-07-16 12:01:24 +0530

    add: lucide-react

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,21 @@
 import React from "react"
+import { useState } from 'react'
+import { Menu, X } from 'lucide-react';
 
 const Navbar:React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+
+
   return (
     <div className="fixed top-0 left-0 h-16 sm:h-20 w-full bg-gray-700 flex justify-center items-center gap-25 text-lg font-medium text-white z-3">
+      {/* Hamburger */}
+        <div className='md:hidden'>
+          <button onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <X size={28} /> : <Menu size={36} />}
+          </button>
+        </div>
+        
         <ul>HOME</ul>
         <ul>SKILLS</ul>
         <ul>PROJECTS</ul>
