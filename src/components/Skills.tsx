e7f1d76c780e6c45ef58aceac6adf90e76350a5a
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-07-22 11:34:57 +0530

    update: text size

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -13,23 +13,23 @@ import React from 'react';
           <h3 className="text-center text-[32px] mt-[20px]">LANGUAGES :</h3>
 
           <div className="flex justify-center items-center gap-4 mt-[20px]">
-            <img src="typescript.png" className="h-[40px]" alt="TypeScript Logo" />
-            <h3 className="text-[30px]">TYPESCRIPT</h3>
+            <img src="typescript.png" className="h-[36px]" alt="TypeScript Logo" />
+            <h3 className="text-[28px]">TYPESCRIPT</h3>
           </div>
 
           <div className="flex justify-center items-center gap-4 mt-[20px]">
-            <img src="javascript.png" className="h-[40px]" alt="JavaScript Logo" />
-            <h3 className="text-[30px]">JAVASCRIPT</h3>
+            <img src="javascript.png" className="h-[36px]" alt="JavaScript Logo" />
+            <h3 className="text-[28px]">JAVASCRIPT</h3>
           </div>
 
           <div className="flex justify-center items-center gap-4 mt-[20px]">
-            <img src="html.png" className="h-[40px]" alt="HTML Logo" />
-            <h3 className="text-[30px]">HTML</h3>
+            <img src="html.png" className="h-[36px]" alt="HTML Logo" />
+            <h3 className="text-[28px]">HTML</h3>
           </div>
 
           <div className="flex justify-center items-center gap-4 mt-[20px]">
-            <img src="css.png" className="h-[40px]" alt="CSS Logo" />
-            <h3 className="text-[30px]">CSS</h3>
+            <img src="css.png" className="h-[36px]" alt="CSS Logo" />
+            <h3 className="text-[28px]">CSS</h3>
           </div>
         </div>
 
@@ -38,23 +38,23 @@ import React from 'react';
           <h3 className="text-center text-[32px] mt-[20px]">TECHNOLOGIES :</h3>
 
           <div className="flex justify-center items-center gap-4 mt-[20px]">
-            <img src="react.png" className="h-[40px]" alt="React Logo" />
-            <h3 className="text-[30px]">REACT</h3>
+            <img src="react.png" className="h-[36px]" alt="React Logo" />
+            <h3 className="text-[28px]">REACT</h3>
           </div>
 
           <div className="flex justify-center items-center gap-4 mt-[20px]">
-            <img src="Tailwind Logo.png" className="h-[28px]" alt="Tailwind Logo" />
-            <h3 className="text-[30px]">TAILWIND CSS</h3>
+            <img src="Tailwind Logo.png" className="h-[24px]" alt="Tailwind Logo" />
+            <h3 className="text-[28px]">TAILWIND CSS</h3>
           </div>
 
           <div className="flex justify-center items-center gap-4 mt-[20px]">
-            <img src="nod.png" className="h-[40px] mt-1" alt="Node.js Logo" />
-            <h3 className="text-[30px]">NODE JS</h3>
+            <img src="nod.png" className="h-[36px] mt-1" alt="Node.js Logo" />
+            <h3 className="text-[28px]">NODE JS</h3>
           </div>
 
           <div className="flex justify-center items-center gap-4 mt-[20px]">
-            <img src="exp.png" className="h-[40px]" alt="Express Logo" />
-            <h3 className="text-[30px]">EXPRESS JS</h3>
+            <img src="exp.png" className="h-[36px]" alt="Express Logo" />
+            <h3 className="text-[28px]">EXPRESS JS</h3>
           </div>
         </div>
       </div>
