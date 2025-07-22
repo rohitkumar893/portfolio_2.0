ce9c8feb0d6f23444cb1c5d4e1dd2b16921409b0
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-07-22 11:42:09 +0530

    fix: responiveness

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -8,13 +8,13 @@ const Certificates: React.FC = () => {
       <div className='flex flex-col flex-wrap justify-evenly sm:gap-[70px]'>
         
         <a href='https://www.coursera.org/account/accomplishments/verify/LFQ94ZUI1Q7K'>
-          <div className='mt-[50px] md:ml-[-350px] mb-[40px] rotate-358 h-[260px] w-[340px] sm:h-[300px] sm:w-[400px] flex items-center justify-center bg-gray-100 rounded-md hover:bg-blue-300 overflow-hidden'>
+          <div className='mt-[50px] md:ml-[-350px] mb-[40px] rotate-0 sm:rotate-357 h-[260px] w-[340px] sm:h-[300px] sm:w-[400px] flex items-center justify-center bg-gray-100 rounded-md hover:bg-blue-300 overflow-hidden'>
             <img src='microsoft.jpg' className='w-full h-full object-cover shadow-sm' />
           </div>
         </a>
 
         <a href='https://www.coursera.org/account/accomplishments/verify/2FGSN01B5TS3'>
-          <div className='mt-[50px] md:mr-[-350px] mb-[40px] rotate-2 h-[260px] w-[340px] sm:h-[300px] sm:w-[400px] flex items-center justify-center bg-gray-100 rounded-md hover:bg-blue-300 overflow-hidden'>
+          <div className='mt-[50px] md:mr-[-350px] mb-[40px] rotate-0 sm:rotate-3 h-[260px] w-[340px] sm:h-[300px] sm:w-[400px] flex items-center justify-center bg-gray-100 rounded-md hover:bg-blue-300 overflow-hidden'>
             <img src='meta.jpg' className='w-full h-full object-cover shadow-sm' />
           </div>
         </a>
