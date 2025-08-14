e7c9240d5932b353680238518df8c30722dd75e9
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-08-14 21:40:08 +0530

    add: animations

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Skills.css'
 
   const Skills: React.FC = () => {
     return (
@@ -12,23 +13,23 @@ import React from 'react';
         <div className="languages">
           <h3 className="text-center text-[32px] mt-[20px]"><b>LANGUAGES :</b></h3>
 
-          <div className="flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
-            <img src="typescript.png" className="h-[36px]" alt="TypeScript Logo" />
+          <div className="spin flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
+            <img src="typescript.png" className="logo h-[36px]" alt="TypeScript Logo" />
             <h3 className="text-[28px]">TYPESCRIPT</h3>
           </div>
 
-          <div className="flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
-            <img src="javascript.png" className="h-[38px]" alt="JavaScript Logo" />
+          <div className="spin flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
+            <img src="javascript.png" className="logo h-[38px]" alt="JavaScript Logo" />
             <h3 className="text-[28px]">JAVASCRIPT</h3>
           </div>
 
-          <div className="flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
-            <img src="html.png" className="h-[36px]" alt="HTML Logo" />
+          <div className="spin flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
+            <img src="html.png" className="logo h-[36px]" alt="HTML Logo" />
             <h3 className="text-[28px]">HTML</h3>
           </div>
 
-          <div className="flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
-            <img src="css.png" className="h-[36px]" alt="CSS Logo" />
+          <div className="spin flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
+            <img src="css.png" className="logo h-[36px]" alt="CSS Logo" />
             <h3 className="text-[28px]">CSS</h3>
           </div>
         </div>
@@ -37,23 +38,23 @@ import React from 'react';
         <div className="technologies">
           <h3 className="text-center text-[32px] mt-[20px]"><b>TECHNOLOGIES :</b></h3>
 
-          <div className="flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
-            <img src="react.png" className="h-[38px]" alt="React Logo" />
+          <div className="spin flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
+            <img src="react.png" className="logo h-[38px]" alt="React Logo" />
             <h3 className="text-[28px]">REACT</h3>
           </div>
 
-          <div className="flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
-            <img src="Tailwind Logo.png" className="h-[24px]" alt="Tailwind Logo" />
+          <div className="spin flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
+            <img src="Tailwind Logo.png" className="logo tw h-[24px]" alt="Tailwind Logo" />
             <h3 className="text-[28px]">TAILWIND CSS</h3>
           </div>
 
-          <div className="flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
-            <img src="nod.png" className="h-[36px] mt-1" alt="Node.js Logo" />
+          <div className="spin flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
+            <img src="nod.png" className="logo h-[36px] mt-1" alt="Node.js Logo" />
             <h3 className="text-[28px]">NODE JS</h3>
           </div>
 
-          <div className="flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
-            <img src="exp.png" className="h-[36px]" alt="Express Logo" />
+          <div className="spin flex justify-center items-center gap-4 mt-[20px] hover:scale-104 transition-transform">
+            <img src="exp.png" className="logo h-[36px]" alt="Express Logo" />
             <h3 className="text-[28px]">EXPRESS JS</h3>
           </div>
         </div>
