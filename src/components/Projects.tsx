23018e96bf16c21619464f110048540a6f517848
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-08-25 20:34:37 +0530

    update: projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Projects: React.FC = () => {
   return (
     <div
-      className='projectscont bg-gray-800 text-white flex justify-center items-center h-auto py-[150px] px-[40px] sm:px-[120px] flex-col w-full' id="projects">
+      className='projectscont bg-gray-800 text-white flex justify-center items-center h-auto py-[150px] px-[40px] sm:px-[120px] flex-col w-[full]' id="projects">
       <h1 className='text-[32px] sm:text-[36px] text-center font-bold'>PROJECTS :</h1>
 
-      <div className='projects flex flex-wrap justify-center gap-10 mt-[50px]'>
-        {/* Project 1 */}
+      <div className='projects flex flex-wrap justify-center gap-10 mt-[50px] sm:w-[80%]'>
+
         <a href="https://roomsley.vercel.app/">
           <div className='mb-[40px] sm:hover:rotate-2 transition h-[290px] w-[340px] sm:h-[280px] sm:w-[340px] flex flex-col justify-between items-center bg-gray-100 rounded-md hover:bg-blue-200 text-black'>
             <img src="roomsley.png" className='w-full shadow-sm rounded-t-md' alt="Roomsley" />
@@ -18,7 +18,16 @@ const Projects: React.FC = () => {
           </div>
         </a>
 
-        {/* Project 2 */}
+        <a href="https://mynotespark.vercel.app/">
+          <div className='mb-[40px] sm:hover:rotate-2 transition h-[290px] w-[340px] sm:h-[280px] sm:w-[340px] flex flex-col justify-between items-center bg-gray-100 rounded-md hover:bg-blue-200 text-black'>
+            <img src="notespark.png" className='w-full shadow-sm rounded-t-md' alt="Resume Builder" />
+            <h1 className='text-[20px] p-[18px] text-gray-700 font-medium'>NoteSpark</h1>
+            <p className='mb-[20px] mt-[-18px] text-gray-600 text-[16px] sm:text-[18px]'>
+              'Reliable note app'
+            </p>
+          </div>
+        </a>
+
         <a href="https://github.com/rohitkumar893/quiqresume">
           <div className='mb-[40px] sm:hover:rotate-2 transition h-[290px] w-[340px] sm:h-[280px] sm:w-[340px] flex flex-col justify-between items-center bg-gray-100 rounded-md hover:bg-blue-200 text-black'>
             <img src="quiq.png" className='w-full shadow-sm rounded-t-md' alt="Resume Builder" />
@@ -29,7 +38,6 @@ const Projects: React.FC = () => {
           </div>
         </a>
         
-        {/* Project 3 */}
         <a href="/">
           <div className='mb-[40px] sm:hover:rotate-2 transition h-[290px] w-[340px] sm:h-[280px] sm:w-[340px] flex flex-col justify-between items-center bg-gray-100 rounded-md hover:bg-blue-200 text-black'>
             <img src="portf.png" className='w-full shadow-sm rounded-t-md' alt="Portfolio" />
