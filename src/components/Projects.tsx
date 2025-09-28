be57e55de77203215f888c26ccc7b0e20afe001e
Rohit Kumar <rkrohitkumar893@gmail.com>
2025-09-28 12:34:01 +0530

    update: projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,41 +9,41 @@ const Projects: React.FC = () => {
       <div className='projects flex flex-wrap justify-center gap-10 mt-[50px] sm:w-[80%]'>
 
         <a href="https://roomsley.vercel.app/">
-          <div className='mb-[40px] sm:hover:rotate-2 transition h-[290px] w-[340px] sm:h-[280px] sm:w-[340px] flex flex-col justify-between items-center bg-gray-100 rounded-md hover:bg-blue-200 text-black'>
+          <div className='mb-[40px] sm:hover:rotate-2 transition h-[258px] w-[340px] flex flex-col justify-center items-center bg-gray-300 rounded-md hover:bg-blue-200 text-black'>
             <img src="roomsley.png" className='w-full shadow-sm rounded-t-md' alt="Roomsley" />
-            <h1 className='text-[20px] p-[18px] text-gray-700 font-medium'>Roomsley</h1>
-            <p className='mb-[20px] mt-[-18px] text-gray-600 text-[16px] sm:text-[18px]'>
+            <h1 className='text-[20px] p-[14px] text-gray-800 font-medium'>Roomsley</h1>
+            <p className='mb-[20px] mt-[-18px] text-gray-700 text-[16px] sm:text-[18px]'>
               'Room listing web-app'
             </p>
           </div>
         </a>
 
         <a href="https://mynotespark.vercel.app/">
-          <div className='mb-[40px] sm:hover:rotate-2 transition h-[290px] w-[340px] sm:h-[280px] sm:w-[340px] flex flex-col justify-between items-center bg-gray-100 rounded-md hover:bg-blue-200 text-black'>
+          <div className='mb-[40px] sm:hover:rotate-2 transition h-[258px] w-[340px] flex flex-col justify-center items-center bg-gray-300 rounded-md hover:bg-blue-200 text-black'>
             <img src="notespark.png" className='w-full shadow-sm rounded-t-md' alt="Resume Builder" />
-            <h1 className='text-[20px] p-[18px] text-gray-700 font-medium'>NoteSpark</h1>
-            <p className='mb-[20px] mt-[-18px] text-gray-600 text-[16px] sm:text-[18px]'>
+            <h1 className='text-[20px] p-[14px] text-gray-800 font-medium'>NoteSpark</h1>
+            <p className='mb-[20px] mt-[-18px] text-gray-700 text-[16px] sm:text-[18px]'>
               'Notes App for daily notes'
             </p>
           </div>
         </a>
 
         <a href="https://github.com/rohitkumar893/quiqresume">
-          <div className='mb-[40px] sm:hover:rotate-2 transition h-[290px] w-[340px] sm:h-[280px] sm:w-[340px] flex flex-col justify-between items-center bg-gray-100 rounded-md hover:bg-blue-200 text-black'>
+          <div className='mb-[40px] sm:hover:rotate-2 transition h-[258px] w-[340px] flex flex-col justify-center items-center bg-gray-300 rounded-md hover:bg-blue-200 text-black'>
             <img src="quiq.png" className='w-full shadow-sm rounded-t-md' alt="Resume Builder" />
-            <h1 className='text-[20px] p-[18px] text-gray-700 font-medium'>QuiqResume</h1>
-            <p className='mb-[20px] mt-[-18px] text-gray-600 text-[16px] sm:text-[18px]'>
+            <h1 className='text-[20px] p-[14px] text-gray-800 font-medium'>QuiqResume</h1>
+            <p className='mb-[20px] mt-[-18px] text-gray-700 text-[16px] sm:text-[18px]'>
               'Generate your resume'
             </p>
           </div>
         </a>
         
         <a href="/">
-          <div className='mb-[40px] sm:hover:rotate-2 transition h-[290px] w-[340px] sm:h-[280px] sm:w-[340px] flex flex-col justify-between items-center bg-gray-100 rounded-md hover:bg-blue-200 text-black'>
+          <div className='mb-[40px] sm:hover:rotate-2 transition h-[258px] w-[340px] flex flex-col justify-center items-center bg-gray-300 rounded-md hover:bg-blue-200 text-black'>
             <img src="portf.png" className='w-full shadow-sm rounded-t-md' alt="Portfolio" />
-            <h1 className='text-[20px] p-[18px] text-gray-700 font-medium' id="portfolio">Portfolio</h1>
-            <p className='mb-[20px] mt-[-18px] text-gray-600 text-[16px] sm:text-[18px]'>
-              'This portfolio'
+            <h1 className='text-[20px] p-[14px] text-gray-800 font-medium' id="portfolio">Portfolio</h1>
+            <p className='mb-[20px] mt-[-18px] text-gray-700 text-[16px] sm:text-[18px]'>
+              'My portfolio'
             </p>
           </div>
         </a>   
