a88a5054b0e874f7156b13f50d256fb364aa128e
Rohit <rkrohitkumar893@gmail.com>
2025-11-19 01:04:24 +0530

    update: background

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer:React.FC = () => {
   return (
-    <section className='h-20 flex justify-center items-start bg-gradient-to-r from-gray-800 to-gray-900'>
+    <section className='h-20 flex justify-center items-start bg-gradient-to-r from-gray-800 to-black'>
         <h1 className='text-gray-400 text-lg'>- Built by Rohit.</h1>
     </section>
   )
